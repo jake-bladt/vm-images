194e9f4c918e5e0a65b3527f4b738cf4ff1e06b4
Jake Bladt <jake@jakebladt.com>
2019-11-11 13:46:15 -0500

    Docker installer script

---
+++ ubuntu-gp/instdocker.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+apt-get update
+apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt-get update
+apt-cache policy docker-ce
+apt-get install docker-ce
