36cab47d1b9cab02f4e12b14c6bd0deb4234931a
Jake Bladt <jake@jakebladt.com>
2019-11-12 10:29:53 -0500

    Use alpine

--- fast-containers/tutpostgres.sh
+++ fast-containers/tutpostgres.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run --name tutorial-postgres -e POSTGRES_PASSWORD=queequeg_1 -p 5432:5432 -d postgres
+docker run --name tutorial-postgres -e POSTGRES_PASSWORD=queequeg_1 -p 5432:5432 -d postgres:12.0-alpine
